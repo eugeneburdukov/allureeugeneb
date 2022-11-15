82a4f1ae22108938fcfbcec0c9484038f9c0a01b
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-16 01:26:24 +0200

    Initial commit

---
+++ src/test/java/AllureReportTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AllureReportTest {
+    @Test
+    public void doAllureTest() {
+        Assert.assertEquals(123, 123);
+    }
+    @Test
+    public void doAllureSecondTest() {
+        Assert.assertEquals(123, 124);
+    }
+}
