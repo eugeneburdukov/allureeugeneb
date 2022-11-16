d441e4710d42872687f2c052c646d3da2d9a58bb
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-16 15:49:52 +0200

    Changed to TestNG, new test data, added steps
    Final valid pom.xml build that works

--- src/test/java/AllureReportTest.java
+++ src/test/java/AllureReportTest.java
@@ -5,19 +5,10 @@ import org.testng.annotations.Test;
 public class AllureReportTest extends AllureReport {
 
     @Test
-    @Step("eugene first test")
     public void toCheckMultiply() {
         AllureReport allureReport = new AllureReportTest();
         int temp1 = doCalculate(10);
         Assert.assertEquals(20, 20);
-    }
-
-    @Test
-    @Step("eugene suka nahui next step")
-    public void toCheckSecondMultiply() {
-        AllureReport allureReport = new AllureReportTest();
-        int temp1 = doCalculate(20);
-        Assert.assertEquals(42, 40);
     }
 
 }
