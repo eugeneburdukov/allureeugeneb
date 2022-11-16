7c4cf4ea419c6aa0de113aa8b6c195b7237a7a1f
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-16 15:54:36 +0200

    Changed to TestNG, new test data, added steps
    Final valid pom.xml build that works
    
    added two methods

--- src/test/java/AllureReportTest.java
+++ src/test/java/AllureReportTest.java
@@ -5,10 +5,17 @@ import org.testng.annotations.Test;
 public class AllureReportTest extends AllureReport {
 
     @Test
-    public void toCheckMultiply() {
+    public void toCheckWar1() {
         AllureReport allureReport = new AllureReportTest();
         int temp1 = doCalculate(10);
         Assert.assertEquals(20, 20);
+    }
+
+    @Test
+    public void toCheckWar2() {
+        AllureReport allureReport = new AllureReportTest();
+        int temp1 = doCalculate(30);
+        Assert.assertEquals(61, temp1);
     }
 
 }
